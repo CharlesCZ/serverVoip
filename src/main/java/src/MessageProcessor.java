e532b8db20fb30ab9f5bf8e1fdb2ab0e81b12bde
CharlesCZ <karakankaczyn@gmail.com>
2019-03-22 23:08:39 +0100

    Initial commit

---
+++ src/main/java/src/MessageProcessor.java
@@ -0,0 +1,8 @@
+package src;
+
+public interface MessageProcessor
+{
+    public void processMessage(String sender, String message);
+    public void processError(String errorMessage);
+    public void processInfo(String infoMessage);
+}
