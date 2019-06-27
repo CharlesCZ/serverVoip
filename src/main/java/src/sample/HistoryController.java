3ae4fb232de4a1e8ff30bc14259f4852ba05df34
CharlesCZ <karakankaczyn@gmail.com>
2019-06-27 14:25:11 +0200

    adding indexes to history

--- src/main/java/src/sample/HistoryController.java
+++ src/main/java/src/sample/HistoryController.java
@@ -1,17 +1,23 @@
 package src.sample;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import src.model.DatabaseVoip;
 import src.model.HistoryConnection;
 import src.model.User;
 
 public class HistoryController {
 
+    @FXML
+    private TableColumn columnHashId;
     @FXML
     private TableColumn columnId;
     @FXML
@@ -30,6 +36,12 @@ public class HistoryController {
 
 
     public void create(User user) {
+        columnHashId.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<HistoryConnection, HistoryConnection>, ObservableValue<HistoryConnection>>() {
+            @Override public ObservableValue<HistoryConnection> call(TableColumn.CellDataFeatures<HistoryConnection, HistoryConnection> p) {
+                return new ReadOnlyObjectWrapper(tableViewId.getItems().indexOf(p.getValue()) + "");
+            }
+        });
+        columnHashId.setSortable(false);
         this.user=user;
         DatabaseVoip d=new DatabaseVoip();
     final ObservableList<HistoryConnection> data=  FXCollections.observableArrayList(d.selectHistoryConnectionByUserId(user.getId()));
